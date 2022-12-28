d9b896633b93aacf73c7211881c513359439f9bb
ningsibuqu <51028975+ningsibuqu@users.noreply.github.com>
2022-12-28 09:01:40 +0800

    Update diy-360T7-hanwckf-2.sh

--- diy/diy-360T7-hanwckf-2.sh
+++ diy/diy-360T7-hanwckf-2.sh
@@ -9,12 +9,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-#修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate   # 定制默认IP
+#修改默认 IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate   # 定制默认 IP
+sed -i 's/ImmortalWrt/TPlink/g' package/base-files/files/bin/config_generate   # 定制默认主机名字
+
 # 移除重复软件包
 #rm -rf feeds/luci/themes/luci-theme-argon
 
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+# 修改 argon 为默认主题，可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Themes
